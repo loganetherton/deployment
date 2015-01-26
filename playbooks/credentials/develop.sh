0375b603bd769c7457bfa11d50c27e53f49a54d4
logan <loganetherton@gmail.com>
2015-01-26 00:15:38 -0500

    Add develop.sh

---
+++ playbooks/credentials/develop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export FACEBOOK_APP_ID=xxxx
+export FACEBOOK_APP_SECRET=xxxx
+export FACEBOOK_APP_REDIRECT=xxxx
+
+export GOOGLE_APP_ID=xxxx
+export GOOGLE_APP_SECRET=xxxx
+export GOOGLE_APP_REDIRECT=xxxx
+
+export TWITTER_APP_ID=xxxx
+export TWITTER_APP_SECRET=xxxx
+export TWITTER_APP_REDIRECT=xxxx
